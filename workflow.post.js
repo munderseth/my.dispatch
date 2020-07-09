1ab311391c1b6cbd39fc568015b2677374071e19
munderseth <marku@s2technologies.com>
2020-07-09 11:49:21 -0700

    file support

--- workflow.post.js
+++ workflow.post.js
@@ -1,13 +1,16 @@
 const { Octokit } = require("@octokit/rest");
+var fs = require('fs');
 
 var token = process.env.GH_TOKEN;
 
 var octokit = new Octokit({ auth: token });
 
 var myinput = {"p1": "one", "p2": "two"};
+var myinputJSON = JSON.stringify(myinput);
 
 var myinputs = { 
  p1: "js",
+ fileinput: myinputJSON
 }
 
 //inputs: JSON.stringify(myinput)
