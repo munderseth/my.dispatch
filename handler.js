47836e35546d7ce5517343858260b720873cc45d
Mark Underseth <marku@s2technologies.com>
2020-07-17 10:53:24 -0700

    Specs (#1)
    * support for workflow_dispatch
    * added spec(s)

--- handler.js
+++ handler.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
+
 function handler(inputs) {
 
     console.log("Handler ..");
     var inputs = JSON.parse(inputs);
-    console.log("p1: ", inputs.p1);
-    console.log("fileinput: ", inputs.fileinput);
+    console.log("p1:", inputs.p1);
+    console.log("p2:", inputs.p2);
+    console.log("filepath:", inputs.file);
+    input = JSON.parse(fs.readFileSync(inputs.file, 'utf8'));
+    console.debug("inputfile:", input);
 }
 
 handler(process.argv[2]);
---
+++ specs/wf.md
@@ -0,0 +1,15 @@
+---
+before:
+  name: github::wf
+  description: example input
+  input:
+    p1: one
+    p2: two
+---
+
+# Spec1
+Lets see
+
+## Doit
+
+* one
